d0c96fee67509515d7493a13517119d342dce2bc
dungpngoc <dungpngoc@gmail.com>
2022-11-30 14:52:22 +0700

    update

--- day02/homeWork_02/src/classScanner.java
+++ day02/homeWork_02/src/classScanner.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class classScanner {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập dữ liệu từ bàn phím");
+//        byte b = sc.nextByte();
+//        short s = sc.nextShort();
+//        int i = sc.nextInt();
+//        long l = sc.nextLong();
+//        float f = sc.nextFloat();
+//        double d= sc.nextDouble();
+//        String str = sc.next();
+//        String str1 = sc.nextLine();
+//        System.out.println("Dữ liệu vừa nhập là : " + str1);
+        System.out.println("Nhập tên : ");
+        String name = sc.nextLine();
+        System.out.println("Nhập tuổi : ");
+//      Cách 1:
+//         Khắc phục trôi lệnh sử dụng Integer.parseInt
+//        int age = Integer.parseInt(sc.nextLine());
+
+//      Cách 2:
+//      Khắc phục trôi lệnh sử dụng nextLine()
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Nhập email : ");
+        String email = sc.nextLine();
+
+        System.out.println("--- Thông tin user ---");
+        System.out.println("Họ : " + name);
+        System.out.println("Tuổi : " + age);
+        System.out.println("Email : " + email);
+
+
 
     }
 }
--- day02/homeWork_02/src/homeWork_day02.java
+++ day02/homeWork_02/src/homeWork_day02.java
@@ -3,26 +3,28 @@ import java.util.Scanner;
 public class homeWork_day02 {
     public static void main(String[] args) {
         //1. Viết chương trình tính chu vi và diện tích của hình chữ nhật với chiều dài và chiều rộng nhập vào từ bàn phím
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập chiều rộng ");
-        double width = sc.nextDouble();
-        System.out.print("Nhập chiều cao ");
-        double height = sc.nextDouble();
-        double area = width * height;
-        System.out.println("Diện tích hình chữ nhật là " + area);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Nhập chiều rộng ");
+//        double width = sc.nextDouble();
+//        System.out.print("Nhập chiều cao ");
+//        double height = sc.nextDouble();
+//        double area = width * height;
+//        System.out.println("Diện tích hình chữ nhật là " + area);
 
         //2. Viết chương trình giới thiệu bản thân gồm các thông tin họ tên, tuổi, giới tính,
         // địa chỉ, với các thông tin cá nhân được nhập vào từ bàn phím
-
-
-
-//        String name = "Phạm Ngọc Dũng";
-//        int age = 28;
-//        String sex = "Nam";
-//        String address = "Hà Nội";
-//        String interets = "chơi thể thao";
-//        System.out.println("Mình tên là "+ name + ",năm nay " + age +" tuổi " + ",giới tính " + sex +
-//                ", hiện tại đang sinh sống và làm việc tại " + address + ", sở thích của tôi là " + interets);
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập họ và tên");
+        String name = sc.nextLine();
+        System.out.println("Nhập tuổi");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Nhập giới tính");
+        String sex = sc.nextLine();
+        System.out.println("Nhập địa chỉ");
+        String address = sc.nextLine();
+        System.out.println("Tôi tên là " + name + ", năm nay " + age + " tuổi " + ", hiện tại đang sinh sống tại "
+                + address);
     }
+}
 
