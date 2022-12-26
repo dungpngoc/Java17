60c4b49a142b054f73541ce879286c5e02b36acc
dungpngoc <117157383+dungpngoc@users.noreply.github.com>
2022-12-26 21:30:48 +0700

    update

--- day12/lesson_12/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import book_management.frontend.ui.BookUI;
-
-public class Main {
-    public static void main(String[] args) {
-        BookUI bookUI = new BookUI();
-        bookUI.run();
-    }
-}
